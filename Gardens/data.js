88a6455cb549f3f5022c183afc382168b1e44e78
cyrus1105 <yuting9811@gmail.com>
2026-05-08 11:39:33 +0800

    0

---
+++ Gardens/data.js
@@ -0,0 +1,231 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-the-ornamental-garden",
+      "name": "The Ornamental Garden",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.8254513072152676,
+        "pitch": 0.0024023547617879615,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.509037599025227,
+          "pitch": 0.07577084727147465,
+          "rotation": 0.7853981633974483,
+          "target": "1-the-serpent-garden"
+        },
+        {
+          "yaw": -0.7868025559128675,
+          "pitch": 0.3590882477015285,
+          "rotation": 0,
+          "target": "4-the-cherry-orchard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-the-serpent-garden",
+      "name": "The Serpent Garden",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.124301793888055,
+        "pitch": 0.14742918157135065,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2236600839615015,
+          "pitch": 0.23104227197562466,
+          "rotation": 0.7853981633974483,
+          "target": "0-the-ornamental-garden"
+        },
+        {
+          "yaw": 2.4596042748985374,
+          "pitch": 0.20668909331787333,
+          "rotation": 0,
+          "target": "3-the-banboo-labirinth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-the-rose-gardon",
+      "name": "The Rose Gardon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.8199288239677784,
+        "pitch": 0.002989756762064033,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4493133956576454,
+          "pitch": 0.39298593597476206,
+          "rotation": 0,
+          "target": "3-the-banboo-labirinth"
+        },
+        {
+          "yaw": -0.05048887054562101,
+          "pitch": 0.13111599606537716,
+          "rotation": 0,
+          "target": "4-the-cherry-orchard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-the-banboo-labirinth",
+      "name": "The Banboo Labirinth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.674580140226012,
+        "pitch": 0.08314656173992852,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0761140252101065,
+          "pitch": 0.4674215781988025,
+          "rotation": 2.356194490192345,
+          "target": "1-the-serpent-garden"
+        },
+        {
+          "yaw": 0.1033855952843048,
+          "pitch": 0.3054287570488263,
+          "rotation": 0,
+          "target": "2-the-rose-gardon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-the-cherry-orchard",
+      "name": "The Cherry Orchard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.06945698043650594,
+        "pitch": -0.0675815590362685,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4764564901090065,
+          "pitch": 0.11770180105093786,
+          "rotation": 0,
+          "target": "0-the-ornamental-garden"
+        },
+        {
+          "yaw": -1.7238042779379619,
+          "pitch": 0.27191749271743504,
+          "rotation": 0,
+          "target": "2-the-rose-gardon"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Gardens",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
